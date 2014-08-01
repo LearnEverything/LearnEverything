aebf34b627127e207155700fd344b47ef8432d51
Paul Emile Gerard <paul1gerard@gmail.com>
2014-08-01 17:50:09 -0500

    "2nd commit working still on trello objectives"

--- collections/exercises.js
+++ collections/exercises.js
@@ -1,1 +1,15 @@
 Exercises = new Meteor.Collection('exercises');
+
+Exercises.allow({
+  insert: function(title) {
+    return true;
+  },
+  //update: function() {
+    //return doc.owner === userId;
+   // return true;
+  //},
+  remove: function(title){
+    return true;
+  },
+  fetch: []
+});
---
+++ server/permissions.js
@@ -0,0 +1,20 @@
+var generalPermissions = {
+  insert: function(name, userId){
+    return true;
+  },
+  update: function(name, userId){
+    //if (doc.userId !== userId) return false;
+    return true;
+  },
+  remove: function(name, userId){
+    return true;
+  }
+};
+
+Exercises.allow(generalPermissions);
+Projects.allow(generalPermissions);
+Syllabi.allow(generalPermissions);
+Topics.allow(generalPermissions);
+UsersExercises.allow(generalPermissions);
+UsersSyllabi.allow(generalPermissions);
+Meteor.users.allow(generalPermissions);
